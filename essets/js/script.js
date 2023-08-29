fe48a1c237cf634ad14c2b917f6ad7576fd2c458
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-08-29 15:04:55 +0000

    Add value in js

--- essets/js/script.js
+++ essets/js/script.js
@@ -1,10 +1,26 @@
 const cards = document.querySelectorAll(".memory-card");
 
-const flipCard = e => {
-    console.log("Event on card click", e.target.parentElement)
-}
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
-cards.forEach (card => {
+const flipCard = e => {
+    const target = e.target.parentElement;
+
+    target.classList.add("flip");
+
+    if (!hasFlippedCard) {
+
+        hasFlippedCard = true;
+        firstCard = target;
+    }else {
+
+        hasFlippedCard = false;
+        secondCard = target;
+    }
+
+
+};
+cards.forEach(card => {
     // Add Event Listener to every card
-    card.addEventListener('clic', flipCard);
-});
+    card.addEventListener("clic", flipCard);
+}); 
