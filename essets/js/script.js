adf92c1b342595afd67135eca1531798fe565e3d
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-08-30 09:05:02 +0000

    Add logic for flipping cards

--- essets/js/script.js
+++ essets/js/script.js
@@ -1,12 +1,16 @@
-const cards = document.querySelectorAll(".memory-card");
+const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let boardLocked = false;
 let firstCard, secondCard;
 
 const flipCard = e => {
+    if (boardLocked) return;
     const target = e.target.parentElement;
 
     target.classList.add("flip");
+
+    console.log("Framework of current card", target.dataset.framework);
 
     if (!hasFlippedCard) {
 
@@ -16,11 +20,30 @@ const flipCard = e => {
 
         hasFlippedCard = false;
         secondCard = target;
+
+        checkForMatch();
     }
 
+};
+
+const checkForMatch = () => {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        firstCard.removeEventListener("Click", flipCard);
+        secondCard.removeEventListener("click", flipCard)
+    } else { 
+        boardLocked = true;
+      setTimeout ( () => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+
+        boardLocked = false;
+        }, 1000);
+
+    }
 
 };
+
 cards.forEach(card => {
     // Add Event Listener to every card
-    card.addEventListener("clic", flipCard);
+    card.addEventListener("click", flipCard);
 }); 
