92b91470252a37bc7b6c984eec4fb9948c8583fb
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-08-30 09:29:35 +0000

    Add random cards

--- essets/js/script.js
+++ essets/js/script.js
@@ -8,6 +8,8 @@ const flipCard = e => {
     if (boardLocked) return;
     const target = e.target.parentElement;
 
+    if (target === firstCard) return;
+
     target.classList.add("flip");
 
     console.log("Framework of current card", target.dataset.framework);
@@ -27,23 +29,36 @@ const flipCard = e => {
 };
 
 const checkForMatch = () => {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        firstCard.removeEventListener("Click", flipCard);
-        secondCard.removeEventListener("click", flipCard)
-    } else { 
-        boardLocked = true;
-      setTimeout ( () => {
+    const isEqual = firstCard.dataset.framework === secondCard.dataset.framework
+
+    isEqual ? disableCards() : unflipCards();
+};
+
+const disableCards = () => {
+
+    firstCard.removeEventListener("Click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+
+}
+
+const unflipCards = () => {
+      boardLocked = true;
+
+      setTimeout (() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
 
         boardLocked = false;
         }, 1000);
+}
 
-    }
 
-};
 
 cards.forEach(card => {
+
     // Add Event Listener to every card
     card.addEventListener("click", flipCard);
+
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    card.style.order = randomIndex;
 }); 
